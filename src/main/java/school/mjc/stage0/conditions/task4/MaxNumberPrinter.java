da145c4d57d93acde25da2a79b6f88dc3bfb7e80
BekoSm <b.smagulov.1999@gmail.com>
2022-07-20 12:41:25 +0600

    Push#3

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,14 +2,18 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        if (first > second && first > third) {
-            System.out.println(first);
-        }
-        else if (first<second && second > third){
-            System.out.println(second);
-        }
-        else {
-            System.out.println(third);
-        }
+
+        int result = first>second? first:second;
+        result = result>third? result:third;
+        System.out.println(result);
     }
 }
+//if (first > second && first > third) {
+//        System.out.println(first);
+//        }
+//        else if (second > first && second > third){
+//        System.out.println(second);
+//        }
+//        else {
+//        System.out.println(third);
+//        }
